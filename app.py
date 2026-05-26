90dee6ec5ceeea3497372cc7100675df1d1f7366
kowshikvellanki00-ctrl <kowshikvellanki00@gmail.com>
2026-05-27 00:29:49 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,1 +1,37 @@
+import streamlit as st
+import google.generativeai as genai
 
+st.title("AI Resume & Job Match Analyzer")
+
+api_key = st.text_input("Enter Gemini API Key", type="password")
+
+if api_key:
+    genai.configure(api_key=api_key)
+
+    model = genai.GenerativeModel('gemini-1.5-flash')
+
+    resume = st.text_area("Paste Resume")
+
+    job_description = st.text_area("Paste Job Description")
+
+    if st.button("Analyze Resume"):
+
+        prompt = f'''
+        Analyze this resume against the job description.
+
+        Give:
+        - Match percentage
+        - Missing skills
+        - ATS suggestions
+        - Final recommendation
+
+        Resume:
+        {resume}
+
+        Job Description:
+        {job_description}
+        '''
+
+        response = model.generate_content(prompt)
+
+        st.write(response.text)
